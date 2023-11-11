80ff3c3fb2996191b8dbdccbb6930f11db79cd73
Stefan Nystrom <stefan.nystrom@omocom.se>
2023-11-11 14:48:21 +0100

    Trying new approach for SE keys.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,68 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+/* ZMK_UNICODE */
+
+#if !defined OS_UNICODE_LEAD
+    #if HOST_OS == 2
+        #define OS_UNICODE_LEAD &macro_press &kp LALT      // macOS/Windows-Alt-Codes
+    #elif HOST_OS == 1
+        #define OS_UNICODE_LEAD &macro_tap &kp LS(LC(U))   // Linux
+    #else
+        #define OS_UNICODE_LEAD &macro_tap &kp RALT &kp U  // Windows + WinCompose (default)
+    #endif
+#endif
+#if !defined OS_UNICODE_TRAIL
+    #if HOST_OS == 2
+        #define OS_UNICODE_TRAIL &macro_release &kp LALT  // macOS/Windows-Alt-Codes
+    #elif HOST_OS == 1
+        #define OS_UNICODE_TRAIL &macro_tap &kp SPACE     // Linux
+    #else
+        #define OS_UNICODE_TRAIL &macro_tap &kp RET       // Windows + WinCompose (default)
+    #endif
+#endif
+
+#define UC_MACRO(name, unicode_bindings) \
+    / { \
+        macros { \
+            name: name { \
+                compatible = "zmk,behavior-macro"; \
+                label = ZMK_HELPER_STRINGIFY(UC_MACRO_ ## name); \
+                wait-ms = <0>; \
+                tap-ms = <0>; \
+                #binding-cells = <0>; \
+                bindings = <OS_UNICODE_LEAD>, <&macro_tap unicode_bindings>, <OS_UNICODE_TRAIL>; \
+            }; \
+        }; \
+    };
+
+#define UC_MODMORPH(name, uc_binding, shifted_uc_binding) \
+    / { \
+        behaviors { \
+            name: name { \
+                compatible = "zmk,behavior-mod-morph"; \
+                label = ZMK_HELPER_STRINGIFY(UC_MORPH_ ## name); \
+                #binding-cells = <0>; \
+                bindings = <uc_binding>, <shifted_uc_binding>; \
+                mods = <(MOD_LSFT|MOD_RSFT)>; \
+            }; \
+        }; \
+    };
+
+#define ZMK_UNICODE_SINGLE(name, L0, L1, L2, L3) \
+    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
+    UC_MODMORPH(name, &name ## _lower, &none)
+
+#define ZMK_UNICODE_PAIR(name, L0, L1, L2, L3, U0, U1, U2, U3) \
+    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
+    UC_MACRO(name ## _upper, &kp U0 &kp U1 &kp U2 &kp U3) \
+    UC_MODMORPH(name, &name ## _lower, &name ## _upper)
+
+/* Swedish letters */
+ZMK_UNICODE_PAIR(sv_ao,   N0, N0,  E, N5,   N0, N0,  C, N5) // Å
+ZMK_UNICODE_PAIR(sv_ae,   N0, N0,  E, N4,   N0, N0,  C, N4) // Ä
+ZMK_UNICODE_PAIR(sv_oe,   N0, N0,  F, N6,   N0, N0,  D, N6) // Ö
+
 #if defined (MIRYOKU_KEYBOARD_CORNE)
 
 #define XXX &none
@@ -11,8 +73,8 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp A \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp A \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &sv_ao \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &sv_ae \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
                K32  K33  K34       K35  K36  K37
 
@@ -20,7 +82,7 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
 
 
 #define MIRYOKU_LAYER_BASE \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, O),     \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,                \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, sv_oe),    \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_MOUSE, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
