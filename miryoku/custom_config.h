f2f544c05f3b437d951c85e7dc8ec15876189e52
Stefan Nystrom <stefan.nystrom@omocom.se>
2023-11-11 16:07:27 +0100

    Just trying random things I'm afraid

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,5 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+#include <behaviors.dtsi>
+
 #include "../zmk-nodefree-config/helper.h"
 #include "../zmk-nodefree-config/international_chars/swedish.dtsi"
 
