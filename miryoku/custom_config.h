455ebe4c6204851b1e1d7aea081afae5099ad8d7
Stefan Nystrom <stefan.nystrom@omocom.se>
2023-11-11 10:12:46 +0100

    A first attemt att configuration

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,26 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#if defined (MIRYOKU_KEYBOARD_CORNE)
+
+#define XXX &none
+
+#define MIRYOKU_LAYERMAPPING_BASE( \
+K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp Å \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp Ä \
+XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
+K32  K33  K34       K35  K36  K37
+
+#endif
+
+
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, Ö),     \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_MOUSE, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
