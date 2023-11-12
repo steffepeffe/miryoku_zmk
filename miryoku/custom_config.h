027f090c5a856538d82787462b90655582de12c4
Stefan Nystrom <stefan.nystrom@omocom.se>
2023-11-12 15:29:39 +0100

    Moving windows key out of the way of a and implementing desktop navigation keys.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,10 +1,13 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 #include <behaviors.dtsi>
-#include "../zmk-nodefree-config/helper.h"
+ #include "../zmk-nodefree-config/helper.h"
 #include "../zmk-nodefree-config/international_chars/swedish.dtsi"
 
 #if defined (MIRYOKU_KEYBOARD_CORNE)
+
+#define dt_left &kp LC(LG(LEFT))
+#define dt_right &kp LC(LG(RIGHT))
 
 #define XXX &none
 
@@ -14,9 +17,20 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &sv_ao \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &sv_ae \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
+     XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &sv_ao \
+     XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &sv_ae \
+&kp LGUI  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
+               K32  K33  K34       K35  K36  K37
+
+#define MIRYOKU_LAYERMAPPING_BASE( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &dt_right \
                K32  K33  K34       K35  K36  K37
 
 #endif
@@ -24,6 +38,12 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,     \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     &sv_oe,    \
+&kp A,             U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     &sv_oe,    \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_MOUSE, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+#define MIRYOKU_LAYER_NAV \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
+U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &dt_left,          &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
