5070a044b2e669f5e1a2bb9d305d155b70ff9c1f
Stefan Nystrom <stefan.nystrom@omocom.se>
2024-02-19 09:48:39 +0100

    Trying a new way of handling swedish characters.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 #include <behaviors.dtsi>
- #include "../zmk-nodefree-config/helper.h"
+#include "../zmk-nodefree-config/helper.h"
 #include "../zmk-nodefree-config/international_chars/swedish.dtsi"
 
 #if defined (MIRYOKU_KEYBOARD_CORNE)
@@ -17,8 +17,8 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-     XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &sv_ao \
-     XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &sv_ae \
+     XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp RA(W) \
+     XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp RA(Q) \
 &kp LGUI  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
                     K32  K33  K34       K35  K36  K37
 
@@ -38,7 +38,7 @@
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,     \
-&kp A,             U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     &sv_oe,    \
+&kp A,             U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     &kp RA(P),    \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
