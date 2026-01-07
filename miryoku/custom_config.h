c195b978bb011714da004de4a224c16cbe30e06c
Stefan Nystrom <stefan.nystrom@omocom.se>
2026-01-07 17:36:18 +0100

    debug step

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,7 +2,6 @@
 // https://github.com/manna-harbour/miryoku
 #include <behaviors.dtsi>
 #include "../zmk-nodefree-config/helper.h"
-#include "../zmk-nodefree-config/international_chars/swedish.dtsi"
 
 #if defined (MIRYOKU_KEYBOARD_CORNE)
 
@@ -11,6 +10,9 @@
 
 #define XXX &none
 
+&uc {
+	default-mode = <UC_MODE_WIN_COMPOSE>;
+};
 
 #define MIRYOKU_LAYERMAPPING_BASE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -18,8 +20,8 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-     &uc UC_SET_LINUX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &sv_ao /* å / Å */ \
-     &uc UC_SET_WIN_COMPOSE  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &sv_ae /* ä / Ä */ \
+     &uc UC_SET_LINUX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &uc 0xE5 0xC5 /* å / Å */ \
+     &uc UC_SET_WIN_COMPOSE  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &uc 0xE4 0xC4 /* ä / Ä */ \
 &kp LGUI  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
                     K32  K33  K34       K35  K36  K37
 
@@ -39,7 +41,7 @@
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,     \
-&kp A,             U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     &sv_oe /* ö / Ö */,    \
+&kp A,             U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     &uc 0xF6 0xD6 /* ö / Ö */,    \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
