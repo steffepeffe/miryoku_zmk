16ee86dbfa1b300ca0c01a7592a45c07e9bfc66c
Stefan Nystrom <stefan.nystrom@omocom.se>
2026-01-08 10:22:39 +0100

    Again with the unicode

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -54,3 +54,9 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 &kp F11,           &kp F4,            &kp F5,            &kp F6,            &kp SLCK,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp F10,           &kp F1,            &kp F2,            &kp F3,            &kp PAUSE_BREAK,   U_NA,              &u_to_U_FUN,       &u_to_U_MEDIA,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp K_APP,         &kp TAB,           &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+&uc {
+	default-mode = <UC_MODE_WIN_COMPOSE>;
+	win-compose-key = <RALT>;
+};
+
