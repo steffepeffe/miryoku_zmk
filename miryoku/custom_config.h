29597f13b80cee661d59ddbdec6a5af268cf9ec1
Stefan Nystrom <stefan.nystrom@omocom.se>
2026-01-08 14:37:17 +0100

    Again with the unicode

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,16 +12,28 @@
 
 #define XXX &none
 
+ZMK_BEHAVIOR(linux_oe, macro,
+	bindings = <&kp INS &kp DQT &kp O>;
+)
+
+ZMK_BEHAVIOR(linux_ae, macro,
+	bindings = <&kp INS &kp DQT &kp A>;
+)
+
+ZMK_BEHAVIOR(linux_ao, macro,
+	bindings = <&kp INS &kp RING>;
+)
+
 #define MIRYOKU_LAYERMAPPING_BASE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-     &uc UC_SET_LINUX_ALT    K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &uc UC_SV_AO /* å / Å */ \
-     &uc UC_SET_WIN_COMPOSE  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &uc UC_SV_AE /* ä / Ä */ \
-     &kp LGUI                K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
-                                       K32  K33  K34       K35  K36  K37
+     &tog U_LINUX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &uc UC_SV_AO /* å / Å */ \
+     XXX          K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &uc UC_SV_AE /* ä / Ä */ \
+     &kp LGUI     K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
+                            K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_LAYERMAPPING_NAV( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -34,12 +46,29 @@
 &kp LGUI  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  dt_right \
                     K32  K33  K34       K35  K36  K37
 
+#define MIRYOKU_LAYERMAPPING_LINUX( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+     &tog U_LINUX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &linux_ao /* å / Å */ \
+     XXX          K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &linux_ae /* ä / Ä */ \
+     &kp LGUI     K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
+                            K32  K33  K34       K35  K36  K37
+
 #endif
 
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,     \
 &kp A,             U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     &uc UC_SV_OE /* ö / Ö */,    \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+#define MIRYOKU_LAYER_LINUX \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,     \
+&kp A,             U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     &linux_oe /* ö / Ö */,    \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
