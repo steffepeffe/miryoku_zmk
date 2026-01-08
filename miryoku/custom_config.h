ab0244e8ae14ea44f23f76831cc37072e4d4fee1
Stefan Nystrom <stefan.nystrom@omocom.se>
2026-01-08 16:08:28 +0100

    Toggle linux with pinky

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -126,7 +126,7 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-     &tog U_LINUX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &linux_ao /* å / Å */ \
+     XXX          K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &linux_ao /* å / Å */ \
      XXX          K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &linux_ae /* ä / Ä */ \
      &kp LGUI     K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp SQT \
                             K32  K33  K34       K35  K36  K37
@@ -136,7 +136,7 @@
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,     \
-&kp A,             U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     &uc UC_SV_OE /* ö / Ö */,    \
+U_MT(U_LINUX, A),  U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     &uc UC_SV_OE /* ö / Ö */,    \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
